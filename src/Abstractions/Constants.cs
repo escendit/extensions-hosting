b76cde2f2063853afbe7d95502e7b4ec1eac4f6d
Simon Novak <25969+snovak7@users.noreply.github.com>
2025-08-11 20:49:33 +0200

    feat: add `Hosting` and `Abstractions` projects with initial dependencies and configuration

---
+++ src/Abstractions/Constants.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the Escendit GmbH under one or more agreements.
+// The Escendit GmbH licenses this file to you under the Apache License 2.0.
+
+namespace Escendit.Extensions.Hosting.Abstractions;
+
+#pragma warning disable CA1034
+
+using System.Diagnostics;
+
+/// <summary>
+/// Constants.
+/// </summary>
+public static class Constants
+{
+    /// <summary>
+    /// Runtime Constants.
+    /// </summary>
+    public static class Runtime
+    {
+        /// <summary>
+        /// Server Constants.
+        /// </summary>
+        public static class Server
+        {
+            /// <summary>
+            /// Runtime Server Source Name.
+            /// </summary>
+            public const string Name = "Escendit.Runtime.Server";
+
+            /// <summary>
+            /// Runtime Server Activity Source.
+            /// </summary>
+            public static readonly ActivitySource Source = new(Name);
+        }
+    }
+}
---
+++ src/Hosting/HostApplicationBuilderExtensions.Orleans.cs
@@ -0,0 +1,77 @@
+﻿// Licensed to the Escendit GmbH under one or more agreements.
+// The Escendit GmbH licenses this file to you under the Apache License 2.0.
+
+namespace Microsoft.Extensions.Hosting;
+
+using Configuration;
+using DependencyInjection;
+using Orleans.Configuration;
+
+/// <summary>
+/// A class that provides extension methods for the HostApplicationBuilder.
+/// Contains methods to extend the functionality of the host application builder
+/// during application configuration and setup.
+/// </summary>
+public static partial class HostApplicationBuilderExtensions
+{
+    /// <summary>
+    /// Configures the host application builder to use a runtime server.
+    /// This method extends the HostApplicationBuilder with functionality
+    /// related to setting up a runtime server during the application configuration.
+    /// </summary>
+    /// <param name="builder">
+    /// The <see cref="HostApplicationBuilder"/> instance to configure.
+    /// </param>
+    /// <returns>
+    /// The configured <see cref="HostApplicationBuilder"/> instance.
+    /// </returns>
+    public static HostApplicationBuilder UseRuntimeServer(this HostApplicationBuilder builder)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        return builder
+            .AddEventSourcingDefaults()
+            .AddHostingDefaults()
+            .AddActivityPropagation();
+    }
+
+    private static HostApplicationBuilder AddEventSourcingDefaults(this HostApplicationBuilder builder)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        return builder
+            .UseOrleans(siloBuilder => siloBuilder
+                .AddLogStorageBasedLogConsistencyProviderAsDefault()
+                .AddStateStorageBasedLogConsistencyProviderAsDefault());
+    }
+
+    private static HostApplicationBuilder AddHostingDefaults(this HostApplicationBuilder builder)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        builder
+            .Services
+            .Configure<ClusterOptions>(configureOptions => builder
+                .Configuration
+                .Bind(configureOptions));
+
+        builder
+            .UseOrleans(siloBuilder =>
+            {
+                var hostingMode = builder
+                    .Configuration
+                    .GetValue<string>("Hosting");
+
+                _ = hostingMode switch
+                {
+                    "Kubernetes" => siloBuilder.UseKubernetesHosting(),
+                    _ => siloBuilder,
+                };
+            });
+        return builder;
+    }
+
+    private static HostApplicationBuilder AddActivityPropagation(this HostApplicationBuilder builder)
+    {
+        return builder
+            .UseOrleans(siloBuilder => siloBuilder
+                .AddActivityPropagation());
+    }
+}
